fe5199fec1c2118717a105893cb59c83f7eec943
Logan Bussell <loganbussell@microsoft.com>
2026-02-18 08:59:03 -0800

    Add deployment pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/azure-pipelines-analyzer/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
